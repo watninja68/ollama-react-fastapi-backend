16cf186503c1af6f366ac0341e3287af0d7e114e
karneeshkar <karneeshkar68@gmail.com>
2025-02-27 05:56:04 +0530

    minor

--- setup.sh
+++ setup.sh
@@ -19,23 +19,7 @@ echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
 # Start the FastAPI backend in the background.
-echo "Starting FastAPI backend..."
 
-
-# Change directory to the React app folder.
-echo "Setting up React frontend..."
-cd fastapi-react-ui
-
-# Install Node.js dependencies if package.json exists.
-if [ -f package.json ]; then
-    npm install
-    echo "Starting React development server..."
-    # npm start  
-    # run this after the script
-else
-    echo "No package.json found in fastapi-react-ui. Skipping React setup."
-fi
-cd ..
 uvicorn app:app --reload --host 0.0.0.0 --port 8081
 echo "backend is running.Run the front end"
 echo "Press Ctrl+C to stop."
